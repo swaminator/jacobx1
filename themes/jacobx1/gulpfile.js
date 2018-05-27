9b2f438da63d3064b15a940afbb2f1fb18f2d081
Jacob Schwind <jacobx1@gmail.com>
2018-05-27 13:27:49 -0400

    Replace pure-css with css grid

---
+++ themes/jacobx1/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp')
+	uglify = require('gulp-uglify'),
+	concat = require('gulp-concat'),
+  minifyCSS = require('gulp-minify-css'),
+  watch = require('gulp-watch'),
+  sass = require('gulp-sass');
+
+gulp.task('compress', function() {
+  gulp.src(['assets/js/jquery.min.js', 'assets/js/jquery.prettysocial.min.js', 'assets/js/rainbow-custom.min.js', 'assets/js/scripts.js'])
+    .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('static/js/'));
+  gulp.src('assets/css/**')
+    .pipe(sass({
+      includePaths: require('node-normalize-scss').includePaths
+    }))
+    .pipe(concat('all.min.css'))
+    .pipe(minifyCSS())
+    .pipe(gulp.dest('static/css/'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('assets/**', ['compress']);
+});
